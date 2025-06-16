8a04e45c8ffd2bdeb44e2bcf282685eabedd33ef
Abdellahneedssome <baqua.abdellah@gmail.com>
2025-06-16 03:32:25 +0100

    .

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,21 +17,11 @@ author = 'BaquaAbdellah & HachimBoua'
 release = 'today'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon'  
-]
-
-
+extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme' # This is where you set the theme!
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -2,7 +2,7 @@
 Gesture & Speech Presentation Control
 =======================================
 
-Welcome to the documentation for the Gesture & Speech Controlled Presentation System.
+Welcome to the documentation for the Gesture & Speech Controlled Presentation System, a computer vision educational project supervised by ENSAM AIDTs chef departement Mr. Masrour.
 
 This project provides a robust, hands-free method for controlling slideshow presentations using a combination of computer vision and speech recognition. The system is designed for a presenter to navigate slides using specific hand gestures and voice commands, eliminating the need for a physical clicker.
 
